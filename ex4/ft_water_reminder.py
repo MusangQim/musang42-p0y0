0c19160e02fc1b3afca90ca3dbf10c99b88aeab5
MusangQim <musangqim03@gmail.com>
2026-03-12 21:40:32 +0800

    give space after :

--- ex4/ft_water_reminder.py
+++ ex4/ft_water_reminder.py
@@ -1,5 +1,5 @@
 def ft_water_reminder():
-    last_water = int(input('Days since last watering:'))
+    last_water = int(input('Days since last watering: '))
     if last_water > 2:
         print('Water the plants!')
     else:
