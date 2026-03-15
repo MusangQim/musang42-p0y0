0037017c416917f143d1af0383d87c35312bf410
MusangQim <musangqim03@gmail.com>
2026-03-15 21:10:19 +0800

    last task

---
+++ ex7/ft_seed_inventory.py
@@ -0,0 +1,6 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str)->None:
+    print(seed_type, quantity, unit)
+
+
+ft_seed_inventory("broccoli", 20, "kg")
+ft_seed_inventory("sawi", "ten", "kg")
